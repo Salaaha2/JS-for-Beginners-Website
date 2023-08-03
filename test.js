49df02dfb5916525b7693948c7fe0f2c2acc4dfd
alykaa <alykaasalaah@Alykaas-MBP.home>
2023-08-03 00:16:41 -0400

    new push

---
+++ test.js
@@ -0,0 +1,4 @@
+alert("Hello, World"); //does not need script because all js have script ending, have to pass on a string ""
+//Java is case sensitive 
+// can read statments left to right stacked side by side
+// order is important!
